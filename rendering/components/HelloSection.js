a75642d4be84eb97d992b1491caadbf7e8589d65
MajorDown <romain.fouillaron@gmx.fr>
2023-10-11 21:18:52 +0200

    ajustement helloSection

--- rendering/components/HelloSection.js
+++ rendering/components/HelloSection.js
@@ -2,16 +2,15 @@ const HelloSection = () => {
   return /*html*/ `
         <section id="helloSection">
             <h2>A propos de moi...</h2>
-            <div id="description">
-                <img class="profil" src="images/profil.jpg" alt="photo" width=300>
-                <p>consectetur nisi nostrum. Fugit, aperiam voluptates a, blanditiis suscipit, 
+            <img class="profil" src="images/profil.jpg" alt="photo" width=300>
+            <p id="description">
+                consectetur nisi nostrum. Fugit, aperiam voluptates a, blanditiis suscipit, 
                 reprehenderit autem sunt quia perspiciatis omnis tempore similique sequi 
                 laboriosam illum nulla accusamus dolorum facere fuga earum inventore corporis 
                 maiores et minima libero? Cumque consectetur atque possimus voluptatum 
                 molestias? Saepe voluptate optio unde? Non molestiae doloribus dolores. 
                 Expedita ducimus atque omnis corrupti dolorem minima veritatis officia possimus
-                </p>
-            </div>
+            </p>
             <div id="technoList">
                 <img src="/images/icons/html.png" alt="html5" />
                 <img src="/images/icons/css.png" alt="css3" />
@@ -27,6 +26,12 @@ const HelloSection = () => {
                 <img src="/images/icons/wordpress.png" alt="wordpress" />
                 <img src="/images/icons/react-native.png" alt="react-native" />
             </div>
+            <p class="centeredText">Vous voulez en savoir plus sur moi ?</p>
+            <nav>
+            <a class="linkBtn" href="#">Mes outils</a>
+            <a class="linkBtn" href="#">Mon Portfolio</a>
+            <a class="linkBtn" href="#">Mes passions</a>
+            </nav>
         </section>
     `;
 };
