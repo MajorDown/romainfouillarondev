19bb476a3e3924ace2a7b0206b6a9db568f482a4
MajorDown <romain.fouillaron@gmx.fr>
2023-10-27 20:35:42 +0200

    fin de seosection

--- rendering/components/SeoSection.js
+++ rendering/components/SeoSection.js
@@ -7,11 +7,34 @@ const SeoSection = () => {
             </div>
             <div class="sectionContent">
                 <div class="content r1">
-                    <p>Il est parfois difficile de s'y retrouver parmis toutes les enseignes et toutes les offres d'hébergement proposés.</p>
-                    <p>Hebergement partagés ou dédié, cloud ou VPS sont autant de termes qui peuvent porter à confusion et vous pousser à l'erreur.</p>
-                    <p>Ensemble, nous étudierons quelle est la meilleur solution d'hébergement pour votre projet.</p>
+                    <p>"Le SEO (Search Engine Optimization) est l'art de faire en sorte que votre site web soit bien placé dans les résultats de recherches" ...dixit Google Bard.</p>
+                    <p>Cette définition me semble correcte. Mais elle résume bien trop la complexité de ce concept pour que l'on puisse se rendre compte des paramètres
+                    sur lesquels celà implique de travailler :</p>
+                    <p>Pertinence du contenu. meta-data. Performance. Bonnes pratiques de codes. Mots-clés. Accessibilité...</p>
+                    <p>Finalement, c'est assez simple : pour le SEO, TOUT COMPTE. Mais si le site parfait n'existe pas, il peut toujours être optimisé, afin de répondre 
+                    aux critères des algorythmes de classement de google.</p>
+                    <p>En résumé, un meilleur SEO, c'est une meilleure visibilité pour votre site.</p>
                 </div>
                 <img class="sectionIcon" src="/images/icons/seo.png" alt="seo">
+            </div>
+            <p class="whatICanDo">Ce que je peut réaliser pour vous :</p>
+            <div class="sectionContent">
+                <div class="content r2">
+                    <h3>un Audit SEO</h3>
+                    <p>Après des mois et des mois de fouinage, de creusage de méninges et de tests, j'ai fini par établir un plan d'audit que j'ai requalifié en 3 étapes : "interne", "externe", et "objectif"</p>
+                    <p>Cette méthode me permet de lister l'ordre de priorité des modifications à apporter de manière beaucoup plus objective (d'ou le nom de la dernière étape)</p>
+                    <p>Pour plus d'informations spécifiques, n'hésitez pas à me contacter !</p>
+                </div>
+            </div>
+            <div class="sectionContent">
+                <div class="content r1">
+                    <h3>un Forfait SEO "Audit + Optimisation"</h3>
+                    <p>Faire un audit, c'est une bonne chose, reste à savoir qui va se charger des modifications.</p>
+                    <p>Si vous ne vous sentez pas d'effectuer vous même les corrections nécessaires, vous avez la possibilité de me mandater afin de m'occuper de la besogne.</p>
+                    <p>Résultats garantis quelques mois plus tard ! </p>
+                    <p>/!\ à noter si toutefois vous préférez vous en occuper : la mise à jour progressive d'un site web doit durer le moins possible dans le temps. 
+                    Google n'aime pas les sites qui changent toutes les semaines leurs infos clés.</p>
+                </div>
             </div>
         </section>
     `;
