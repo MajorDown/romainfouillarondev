d780cd4948aa05739386d22da784c722b4613cb1
MajorDown <romain.fouillaron@gmx.fr>
2023-11-05 21:59:27 +0100

    senDmail mis en forme et pret à envoyer la requète

--- public/scripts/SendMail.js
+++ public/scripts/SendMail.js
@@ -1,5 +1,46 @@
 const SendMail = () => {
-  return <div>formulaire de mail</div>;
+  const [mail, setMail] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [message, setMessage] = React.useState("");
+  const [errMessage, setErrMessage] = React.useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <p>...ou sinon, envoyez-moi un message via ce formulaire</p>
+      <form onSubmit={(event) => handleSubmit(event)}>
+        <label htmlFor="senderMail">Renseignez votre adresse mail :</label>
+        <input
+          type="email"
+          id="senderMail"
+          placeholder="votre mail"
+          value={mail}
+          onChange={(event) => setMail(event.target.value)}
+        />
+        <label htmlFor="senderName">Renseignez votre identité : </label>
+        <input
+          type="text"
+          id="senderName"
+          placeholder="Nom, prénom, fonction..."
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <label htmlFor="senderMessage">Ecrivez votre message :</label>
+        <textarea
+          id="senderMessage"
+          cols="40"
+          rows="20"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        ></textarea>
+        <p style={{ color: "red" }}></p>
+        <button type="submit">Envoyer</button>
+      </form>
+    </div>
+  );
 };
 
 const sendMailDiv = document.getElementById("sendMail");
